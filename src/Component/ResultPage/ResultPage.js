bb5b372b95bcf8154dbe382341571d130836bf50
sachinsingh800 <sachinsinghgnc@gmail.com>
2023-03-12 20:52:37 +0530

    delete data from loacal

--- src/Component/ResultPage/ResultPage.js
+++ src/Component/ResultPage/ResultPage.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import style from "./ResultPage.module.css"
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 function ResultPage() {
+    
     const dataPackage=JSON.parse(localStorage.getItem("packageData")) || []
+
+    function handleDelete( index){
+        const newPackage=dataPackage.filter((item,i)=>
+        i !== index
+        )
+        localStorage.setItem("packageData",JSON.stringify(newPackage))
+    }
+    
   return (
     <div className={style.main}>
       <h1>Welcome to the favroite NPM Packages</h1>
          <div className={style.container}>
-        {dataPackage.map((item)=>
-        <div className={style.list}>
+        {dataPackage.map((item,index)=>
+        <div key={index} className={style.list}>
              <li>{item.name}</li>
              <div className={style.iconlist}>
-               <RemoveRedEyeIcon/>
-               <EditIcon />
-               <DeleteIcon />
+              <span ><RemoveRedEyeIcon/></span> 
+              <span><EditIcon /></span>
+              <span onClick={()=>handleDelete(index)}>< DeleteIcon /></span>
              </div>
         </div>
            
